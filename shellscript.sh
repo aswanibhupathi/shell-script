3f2e25ebf85072f853b3476d53ad64efe8abc371
aswanibhupathi <asu.vinay@gmail.com>
2024-12-07 17:13:29 +0530

    practice

--- shellscript.sh
+++ shellscript.sh
@@ -2,8 +2,8 @@
 
 MOVIES=("rrr","dj","pj")
 
-echo "Movies are : ${MOVIES[0]}"
+echo "Movies are : ${MOVIES[0}"
 
-echo "Movies are : ${MOVIES[1]}"
+echo "Movies are : ${MOVIES[1}"
 
-echo "Movies are : ${MOVIES[@]}"
+echo "Movies are : ${MOVIES[@}"
