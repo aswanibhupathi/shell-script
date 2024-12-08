8843a544283f637bf6ac3e4232e6778a033a6e08
aswanibhupathi <asu.vinay@gmail.com>
2024-12-08 20:08:13 +0530

    practice

--- shellscript.sh
+++ shellscript.sh
@@ -4,34 +4,45 @@ TIMESTAMP=$(date | awk '{print $1 $2 $3 $4}')
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME_$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-USERID=$(id -u)
 
-if [ $USERID -eq 0 ]
-then
-    echo "You are super user"
-else
-    echo "You are not super user to run this"
-fi
+echo $R FAILURE $N
 
-VALIDATE(){
-if [ $? -ne 0 ]
-then
-    echo "$1 installing failure"
-    exit 1
-fi
+echo $Y STATUS $N
 
-}
+echo $G SUCESS $N
 
-dnf install mysql-server -y &>>$LOGFILE
+# USERID=$(id -u)
 
-VALIDATE MYSQL
+# if [ $USERID -eq 0 ]
+# then
+#     echo "You are super user"
+# else
+#     echo "You are not super user to run this"
+# fi
 
-systemctl enable mysqld
+# VALIDATE(){
+# if [ $? -ne 0 ]
+# then
+#     echo "$1 installing failure"
+#     exit 1
+# fi
 
-systemctl start mysqld
+# }
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+# dnf install mysql-server -y &>>$LOGFILE
+
+# VALIDATE MYSQL
+
+# systemctl enable mysqld
+
+# systemctl start mysqld
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1
 
 
 
